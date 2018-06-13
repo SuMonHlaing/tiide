22a723cf9a2b7bc19c6875a043d9111b51a21709
sumonhlaing <sumonhlaing1997@gmail.com>
2018-06-13 16:51:36 +0630

    hello

--- hello.py
+++ hello.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World"
+@app.route("/sumonhlaing")
+def smh():
+    return "Su Mon Hlaing"
+if __name__ == "__main__":
+    app.run()
